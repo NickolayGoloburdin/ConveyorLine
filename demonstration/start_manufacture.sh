87242cb79af8439b7f9c4d7a827c6743854ad5d0
NickolayGoloburdin <nickolaygoloburdin@mail.ru>
2020-11-28 11:42:34 +0300

    remove dirs

--- demonstration/start_manufacture.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-source /opt/ros/melodic/setup.bash
-roslaunch /home/root/manufacture_line/src/manufacture_line_ros/manufacture_line/angle_arm_pkg_test/launch/angle_arm_control.launch 
-
-
